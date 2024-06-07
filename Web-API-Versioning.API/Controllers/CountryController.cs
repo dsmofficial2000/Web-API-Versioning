271e678b2bd34b75efd269f16bee1209fcad2816
dsmofficial2000 <dsmofficial2000@gmail.com>
2024-06-07 12:38:13 +0530

    Implement Versioning By Nuget Package - Proffered Approach

--- Web-API-Versioning.API/Controllers/CountryController.cs
+++ Web-API-Versioning.API/Controllers/CountryController.cs
@@ -6,22 +6,48 @@ namespace Web_API_Versioning.API.V1.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
     public class CountryController : ControllerBase
     {
+        //https://localhost:7092/api/Country?api-version=1.0
+        [MapToApiVersion("1.0")]
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult GetV1()
         {
             var countryDomainModel = CountriesData.Get();
 
             //Map Domain to DTO
-            var response = new List<CountryDTO>();
+            var response = new List<CountryDtoV1>();
 
             foreach(var countryDomain  in countryDomainModel)
             {
-                response.Add(new CountryDTO
+                response.Add(new CountryDtoV1
                 {
                     Id = countryDomain.Id,
                     Name = countryDomain.Name,
+                });
+            }
+            return Ok(response);
+        }
+
+
+        //https://localhost:7092/api/Country?api-version=2.0
+        [MapToApiVersion("2.0")]
+        [HttpGet]
+        public IActionResult GetV2()
+        {
+            var countryDomainModel = CountriesData.Get();
+
+            //Map Domain to DTO
+            var response = new List<CountryDtoV2>();
+
+            foreach (var countryDomain in countryDomainModel)
+            {
+                response.Add(new CountryDtoV2
+                {
+                    Id = countryDomain.Id,
+                    CountryName = countryDomain.Name,
                 });
             }
             return Ok(response);
